c518e831bfa8dbf7c82ba2f4330a1594eed597bb
zlipuhina <reptiliaanna@gmail.com>
2024-09-19 19:20:51 +0300

    KS-2-4 buff on material (corrected const and variables)

--- src/main/kotlin/lesson_2/lesson2_task3.kt
+++ src/main/kotlin/lesson_2/lesson2_task3.kt
@@ -4,9 +4,10 @@ fun main(){
     val hoursCheckOut = 9
     val minuteCheckOut = 39
     val minutesOnTheWay = 457
-    val hoursOnTheWay = 457/60
-    val minutesForHours = 457%60
-    val arrivalTimeHours = hoursOnTheWay + hoursCheckOut + ((minuteCheckOut + minutesForHours) / 60)
-    val arrivalTimeMinutes = (minuteCheckOut + minutesForHours)%60
+    val hoursOnTheWay = minutesOnTheWay / STANDART_TIME
+    val minutesForHours = minutesOnTheWay % STANDART_TIME
+    val arrivalTimeHours = hoursOnTheWay + hoursCheckOut + ((minuteCheckOut + minutesForHours) / STANDART_TIME)
+    val arrivalTimeMinutes = (minuteCheckOut + minutesForHours) % STANDART_TIME
     println("Время прибытия поезда: $arrivalTimeHours:$arrivalTimeMinutes")
 }
+const val STANDART_TIME = 60
