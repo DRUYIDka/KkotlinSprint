eee0de6da84b5816387489a9731244212f59e2cc
zlipuhina <reptiliaanna@gmail.com>
2024-09-20 16:32:28 +0300

    KS-3-1 greeting for the whole day

---
+++ src/main/kotlin/lesson_3/lesson3_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main(){
+    val nameUser = "Leni";
+
+    var greeting = "Добрый день";
+    //println("$greeting, $nameUser!"); Приветствие для дня
+
+    //Приветствие для вечера
+    greeting = "Добрый вечер";
+    println("$greeting, $nameUser!");
+
+}
