212f13fedfab67682e1eff10ec0cece0208c3e54
zlipuhina <reptiliaanna@gmail.com>
2024-09-22 18:50:50 +0300

    KS-3-3 multiplication table

---
+++ src/main/kotlin/lesson_3/lesson3_task3.kt
@@ -0,0 +1,16 @@
+package org.example.lesson_3
+
+fun main(){
+    val numberForTable = 5
+    println("""
+        $numberForTable x 1 = ${numberForTable * 1}
+        $numberForTable x 2 = ${numberForTable * 2}
+        $numberForTable x 3 = ${numberForTable * 3}
+        $numberForTable x 4 = ${numberForTable * 4}
+        $numberForTable x 5 = ${numberForTable * 5}
+        $numberForTable x 6 = ${numberForTable * 6}
+        $numberForTable x 7 = ${numberForTable * 7}
+        $numberForTable x 8 = ${numberForTable * 8}
+        $numberForTable x 9 = ${numberForTable * 9}
+        """)
+}
